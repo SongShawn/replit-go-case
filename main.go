52855eea8f25e59efd2227f47d5feab67f767baf
宋哈哈哈哈哈哈 <SongShawn@users.noreply.github.com>
2022-04-19 09:23:57 +0000

    format

--- main.go
+++ main.go
@@ -5,19 +5,19 @@ import "fmt"
 func main() {
 	var a int
 
-    a = 7
+	a = 7
 
-    switch {
-        case a > 0 && a < 4:
-        fmt.Println("a>0 and a<4")
+	switch {
+	case a > 0 && a < 4:
+		fmt.Println("a>0 and a<4")
 
-        case a >= 5 && a < 10:
-        fmt.Println("a>=5 and a<10")
+	case a >= 5 && a < 10:
+		fmt.Println("a>=5 and a<10")
 
-        case a>=10 && a < 20:
-        fmt.Println("a>=10 and a < 20")
-        
-        default:
-        fmt.Println("default")
-    }
+	case a >= 10 && a < 20:
+		fmt.Println("a>=10 and a < 20")
+
+	default:
+		fmt.Println("default")
+	}
 }
