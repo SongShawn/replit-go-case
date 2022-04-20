0c04dce2d8cc50479f25a50a2fd785ce4dcdb4fa
宋哈哈哈哈哈哈 <SongShawn@users.noreply.github.com>
2022-04-20 03:33:21 +0000

    add gin lib

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 )
 
 func main() {
-	httpGetBaiduPage()
+	ginCase()
+}
+
+func ginCase() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong by sxn",
+		})
+	})
+    
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func testSwitchCase1() {
