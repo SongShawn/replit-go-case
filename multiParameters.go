f41f592dff793a8fe347d14e24fa33a584b6fa2d
宋哈哈哈哈哈哈 <SongShawn@users.noreply.github.com>
2022-12-08 00:53:53 +0000

    add multiParamters

---
+++ multiParameters.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func logWrap1(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func logWrap(format string, args ...interface{}) {
+    format += ", %s.\n"
+    args = append(args, "100x")
+    
+    logWrap1(format, args...)
+}
+
+func main() {
+	logWrap("%d %d", 1, 2)
+}
