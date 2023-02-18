d90103c803d141eaf73567ab59d4994a59f23467
宋哈哈哈哈哈哈 <SongShawn@users.noreply.github.com>
2023-02-18 08:08:13 +0000

    两个切片共享相同的底层内存

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	a := []string{"a", "b"}
-    var b []string
-	fmt.Printf("a %s\n", a)
-    fmt.Printf("b %s\n", b)
+	a := make([]int, 0, 10)
+    fmt.Println(len(a), cap(a))
+    a = append(a, 100)
+    fmt.Println(len(a), cap(a))
+    fmt.Println(a)
+
+    // b 和 a 共享同一个底层内存
+    b := a[:0]
+    fmt.Println(len(b), cap(b))
+    b = append(b, 200)
+    fmt.Println(a)
+    fmt.Println(b)
 }
